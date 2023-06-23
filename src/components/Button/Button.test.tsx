d5495ff5346b673a38587844470c938c23fe0748
ivysun777 <114521145+ivysun777@users.noreply.github.com>
2023-06-22 21:15:16 -0500

    update background test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Button from "./Button";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
 
 describe("<Button />", () => {
-  it("should be visible", () => {
+  test("should be visible", () => {
     render(<Button />);
 
     const button = screen.getByTestId("Button");
@@ -12,11 +14,9 @@ describe("<Button />", () => {
     expect(button).toBeVisible();
   });
 
-  it("should change background color on disabled state", () => {
-    render(<Button disabled={true} />);
+  test("should change background color on disabled state", () => {
+    const tree = renderer.create(<Button disabled={true} />).toJSON();
 
-    const button = screen.getByTestId("Button");
-
-    expect(button).toHaveStyle("background-color:ButtonFace");
+    expect(tree).toHaveStyleRule("background", "#aaaaaa");
   });
 });
