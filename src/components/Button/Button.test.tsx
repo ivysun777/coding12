c843774efbcf5da94ef7bfc51fdd96bd20acf729
ivysun777 <114521145+ivysun777@users.noreply.github.com>
2023-06-22 20:31:49 -0500

    add tests for Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Button from './Button';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Button from "./Button";
 
-describe('<Button />', () => {
-  test('it should mount', () => {
+describe("<Button />", () => {
+  it("should be visible", () => {
     render(<Button />);
-    
-    const button = screen.getByTestId('Button');
 
-    expect(button).toBeInTheDocument();
+    const button = screen.getByTestId("Button");
+
+    expect(button).toBeVisible();
+  });
+
+  it("should change background color on disabled state", () => {
+    render(<Button disabled={true} />);
+
+    const button = screen.getByTestId("Button");
+
+    expect(button).toHaveStyle("background-color:ButtonFace");
   });
 });
